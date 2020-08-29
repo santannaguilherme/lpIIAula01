bf226a7475995523069af43de1e54bb1e8925fd8
Guilherme Sant'Anna <guilherme.sanntana@gmail.com>
2020-08-29 12:48:18 -0300

    Implementação do código

--- src/br/com/ftt/lpii/person/PersonaOne.java
+++ src/br/com/ftt/lpii/person/PersonaOne.java
@@ -1,6 +1,10 @@
 package br.com.ftt.lpii.person;
 
 public class PersonaOne extends Person implements IPersonActions{
+    public PersonaOne(String name, int age) {
+        super(name, age);
+    }
+
     @Override
     public String hello() {
         return "Oi";
--- src/br/com/ftt/lpii/person/PersonaTwo.java
+++ src/br/com/ftt/lpii/person/PersonaTwo.java
@@ -1,6 +1,10 @@
 package br.com.ftt.lpii.person;
 
 public class PersonaTwo  extends Person implements IPersonActions{
+    public PersonaTwo(String name, int age) {
+        super(name, age);
+    }
+
     @Override
     public String hello() {
         return "Olá";
